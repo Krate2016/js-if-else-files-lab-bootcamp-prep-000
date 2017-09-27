62c8796621fd4f1cca596bd56c9f86f6f67a3056
Renee Sarmiento <rsarmiento5a@gmail.com>
2017-09-27 00:48:36 +0000

    automatically backed up by learn

--- other_file.js
+++ other_file.js
@@ -2,5 +2,12 @@ console.log(name)
 var index = require("./index.js")
 console.log(index.name)
 var index = require("./index.js")
- 
+
 console.log(index.name)
+if (name === "Susan") {
+    console.log("The name is correct")
+} else {
+    console.log("Expected the name to be Susan")
+}var index = require("./index.js")
+console.log(index.name)
+var index = require("./index.js")
